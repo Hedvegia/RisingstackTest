56f4a3ca8264fe56481322864b1fcb0ca56c5c07
Germus Hédi <hedi9507@gmail.com>
2018-06-10 21:43:14 +0200

    comments

--- model/config.js
+++ model/config.js
@@ -1,3 +1,5 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://Hedvegia:Hariel1995@ds241530.mlab.com:41530/users');
+
+//I used mLab, because locally I couldn't use the mongodb.
